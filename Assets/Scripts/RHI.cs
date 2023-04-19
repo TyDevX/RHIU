3f1fb3898fb69eddc6a46e0ae02115f53e0d1eea
SnesFX <108774265+YukiMD@users.noreply.github.com>
2023-04-19 08:32:18 -0400

    woo save function

--- Assets/Scripts/RHI.cs
+++ Assets/Scripts/RHI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,12 +10,14 @@ public class RHI : MonoBehaviour
 {
     public RawImage imageDisplay;
     public Button getImageButton;
+    public Button saveImageButton;
 
     private const string url = "https://konachan.com/post.json?limit=1&tags=+uncensored+-pool:309+order:random+rating:explicit";
 
     private void Start()
     {
         getImageButton.onClick.AddListener(() => StartCoroutine(GetImage()));
+        saveImageButton.onClick.AddListener(() => SaveImage());
     }
 
     private IEnumerator GetImage()
@@ -46,6 +49,23 @@ public class RHI : MonoBehaviour
                 imageDisplay.gameObject.SetActive(true);
             }
         }
+    }
+
+    private void SaveImage()
+    {
+        Texture2D texture = (Texture2D)imageDisplay.texture;
+        byte[] bytes = texture.EncodeToPNG();
+
+        string savePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/RHIU/";
+        if (!Directory.Exists(savePath))
+        {
+            Directory.CreateDirectory(savePath);
+        }
+
+        string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".png";
+        File.WriteAllBytes(savePath + fileName, bytes);
+
+        Debug.Log("Image saved to " + savePath + fileName);
     }
 
     [Serializable]
---
+++ Changelog.md
@@ -0,0 +1,7 @@
+# Release 1.0
+
+* Main release, the app does what it's supposed to do lmao.
+
+# Release 1.1
+
+* Added Save Function. The image gets saved to  "C:\Users\the current user\AppData\Roaming\RHIU"
